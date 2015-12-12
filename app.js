d269312cb56fa93fa5bda5fafda4f870637c1e25
Anton Kravchenko <antosha.kravchenko@gmail.com>
2015-12-12 22:15:08 +0200

    Basic restify project template.

---
+++ app.js
@@ -0,0 +1,40 @@
+var restify = require('restify');
+var nconf = require('nconf');
+var bunyan = require('bunyan');
+
+var package_json = require('./package.json');
+
+nconf
+	.argv()
+	.env('__')
+	.file({ file : './defaults.json'});
+
+var log = bunyan.createLogger({
+	name : package_json['name'],
+	streams : [
+	 	{
+	        level: 'info',
+	        stream: process.stdout 
+	    },
+	    {
+	        level: nconf.get('log:level'),
+	        type: 'rotating-file',
+	        path: nconf.get('log:path')
+	    }
+	]
+});
+
+
+var server = restify.createServer({
+	name: package_json['name'],
+	version: package_json['version']
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+ 
+log.info('Starting node_notes sever');
+
+server.listen(nconf.get('port'), function () {
+	console.log('%s listening at %s', server.name, server.url);
+});
