0219e5546aad926c68bfd8230f6c338b59677e5b
Anton Kravchenko <antosha.kravchenko@gmail.com>
2015-12-13 15:13:15 +0200

    Move to express.

--- README.md
+++
@@ -1,2 +0,0 @@
-# node_notes
-# node_notes
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var restify = require('restify');
-var nconf = require('nconf');
-var bunyan = require('bunyan');
+var express = require('express');
 var session = require('express-session');
+var bodyParser = require('body-parser');
+var bunyan = require('bunyan');
+var nconf = require('nconf');
 
 var package_json = require('./package.json');
 
@@ -9,77 +10,74 @@ var init_api = require('./lib/api.js');
 var Router = require('./lib/Router');
 
 nconf
-	.argv()
-	.env('__')
-	.file({ file : './defaults.json'});
+    .argv()
+    .env('__')
+    .file({file: './defaults.json'});
 
 var log = bunyan.createLogger({
-	name : package_json['name'],
-	streams : [
-	 	{
-	        level: 'info',
-	        stream: process.stdout 
-	    },
-	    {
-	        level: nconf.get('log:level'),
-	        type: 'rotating-file',
-	        path: nconf.get('log:path')
-	    }
-	]
+    name: package_json['name'],
+    streams: [
+        {
+            level: 'info',
+            stream: process.stdout
+        },
+        {
+            level: nconf.get('log:level'),
+            type: 'rotating-file',
+            path: nconf.get('log:path')
+        }
+    ]
 });
 
-init_api(nconf, log, function(error, api){
-	var server = restify.createServer({
-		name: package_json['name'],
-		version: package_json['version']
-	});
+init_api(nconf, log, function (error, api) {
+    var app = express();
 
-	server.on('uncaughtException', function (req, res, route, err) {
+    app.on('uncaughtException', function (req, res, route, err) {
         var a_log = req.log ? req.log : log;
         a_log.error(err);
-        
+
         res.send(500, {
             status: 'server_error',
             message: err.message
         });
     });
 
-	server.use(restify.bodyParser());
-	server.use(restify.queryParser());
-	server.use(session({
-       	secret: nconf.get('security:session_secret'),
-       	name: 'user_session',
-       	rolling: true,
-       	resave: false,
-       	saveUninitialized: false,
-       	cookie: {
-       		maxAge: 12 * 24 * 60 * 60 * 1000
-       }
-   	})); 
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-	var router = new Router(server, {
-		target : api,
-		log : log
-	});
+    app.use(session({
+        secret: nconf.get('security:session_secret'),
+        name: 'user_session',
+        rolling: true,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 12 * 24 * 60 * 60 * 1000
+        }
+    }));
 
-	router.post('/login', {
-	   parameters: {
-	       username: router.String,
-	       password: router.String
-	   },
-	   call: [ api.login, 'username', 'password' ]
-	});
-	
-	router.post('/register', {
-	   parameters: {
-	       username: router.String,
-	       email: router.String,
-	       password: router.String
-	   },
-	   call: [ api.register, 'username', 'email', 'password' ]
-	});
+    var router = new Router(app, {
+        target: api,
+        log: log
+    });
 
-	server.listen(nconf.get('port'), function () {
-		console.log('%s listening at %s', server.name, server.url);
-	});
+    router.post('/login', {
+        parameters: {
+            username: router.String,
+            password: router.String
+        },
+        call: [api.login, 'username', 'password']
+    });
+
+    router.post('/register', {
+        parameters: {
+            username: router.String,
+            email: router.String,
+            password: router.String
+        },
+        call: [api.register, 'username', 'email', 'password']
+    });
+
+    app.listen(nconf.get('port'), function () {
+        console.log('Listening at %s', nconf.get('port'));
+    });
 });
